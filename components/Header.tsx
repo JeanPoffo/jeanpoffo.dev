517e63de47b0bd191a524d49c4e13625f72b79b8
Jean Poffo <jean.poffo0@gmail.com>
2024-02-18 21:43:10 -0300

    chore: add logo icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,13 +11,9 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
+            <div className="mr-3 h-6 text-2xl font-semibold sm:block">
+              {siteMetadata.headerTitle}
+            </div>
           </div>
         </Link>
       </div>
